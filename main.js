2069b5d0a3341de03343f2e2c78c028508d3b736
oirom <yamauchi.morio853@mail.kyutech.jp>
2020-06-17 01:00:21 +0900

    set buttons to change contents

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ var app = new Vue({
         color: 'red',
         bg: 'bg-blue',
         counter: 0,
+        title: 'none',
+        description: 'none',
         contentObjects: {
-            title: ['SKILL', 'LINK'],
-            description: ['aaa', 'bbb']
+            titles: ['SKILL', 'LINK'],
+            descriptions: ['aaa', 'bbb']
         }
     },
     computed: {
@@ -26,8 +28,16 @@ var app = new Vue({
         doubleCounterMethod: function() {
             return this.counter *2;
         },
-        changeContent: function(event) {
+        changeContent: function(_arg, $event) {
+            this.counter += 1;
+            if (_arg == 'a') {
+                this.title = this.contentObjects.titles[0];
+                this.description = this.contentObjects.descriptions[0];
+            } else {
+                this.title = this.contentObjects.titles[1];
+                this.description = this.contentObjects.descriptions[1];
+            }
             
-        }
+        },
     }
 });
