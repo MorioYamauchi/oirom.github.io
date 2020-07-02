bb1925819252483c6db2d545b719d9e9edf8f24c
oirom <yamauchi.morio853@mail.kyutech.jp>
2020-07-02 12:02:15 +0900

    create GameBoy-Like page

---
+++ test.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: 'こんにちわ',
+        isActive: true,
+        color: 'red',
+        bg: 'bg-blue',
+        counter: 0,
+        title: 'title',
+        content: 'content',
+        contentObjects: {
+            titles: ['SKILL', 'LINK'],
+            descriptions: ['aaa', 'bbb']
+        },
+        links: [
+            { name : 'Twitter' , url : 'https://twitter.com/cumentarydo' },
+            { name : 'GitHub' , url : 'https://github.com/oirom' },
+            { name : 'AtCoder', url : 'https://atcoder.jp/users/oirom0528' }
+        ],
+        now: "00:00:00"
+    },
+    computed: {
+        classObject: function() {
+            return { 
+                red: this.isActive,
+                'bg-blue': !this.isActive
+            }
+        }
+    },
+    methods: {
+        countUp: function() {
+            this.counter += 1;
+        },
+        doubleCounterMethod: function() {
+            return this.counter *2;
+        },
+        changeContent: function(_arg, $event) {
+            if (_arg == 'a') {
+                this.title = this.contentObjects.titles[0];
+                this.content = this.contentObjects.descriptions[0];
+            } else if (_arg=='b') {
+                this.title = this.contentObjects.titles[1];
+                this.content = this.contentObjects.descriptions[1];
+            }
+        },
+        getTime: function() {
+            var date = new Date();
+            this.now = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        }
+    }
+});
